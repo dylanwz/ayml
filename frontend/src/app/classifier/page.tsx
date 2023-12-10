75eeea1efa0f6577ebe875facf34ed8e81c6925b
Dylan <inutuzumab@gmail.com>
2023-12-10 11:49:46 +1100

    scaffold

---
+++ frontend/src/app/classifier/page.tsx
@@ -0,0 +1,8 @@
+export default function Architecture() {
+    return (
+      <div className="flex flex-row w-screen h-screen bg-black">
+        <span className="text-white text-9xl">TO-DO</span>
+      </div>
+    );
+  }
+  
